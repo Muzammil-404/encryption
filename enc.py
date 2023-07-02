cf3adc4aa6ce52e53c7af154abe4597ac2647769
Muhammad Muzammil <122248440+Muzammil-404@users.noreply.github.com>
2023-07-02 08:58:30 +0500

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,17 +1,5 @@
-import os,platform
-
-os.system('git pull')
-
- 
-
-x=platform.architecture()[0]
-
-if x=="32bit":
-
-    print('Sorry 32 Bit Not Supported...')
-
-elif x=="64bit":
-
-
-
+import os, sys
+try:
     __import__("encryption")
+except Exception as e:
+    exit(str(e))
